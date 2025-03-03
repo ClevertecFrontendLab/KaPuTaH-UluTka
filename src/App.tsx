7a672838ea5413207984003acbecb533c0195965
KaPuTaH-UluTka <lexas427@gmail.com>
2025-03-04 00:26:07 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ function App() {
     const [count, setCount] = useState(0);
     return (
         <>
-            <div>
-            </div>
             <h1>Vite + React</h1>
             <div className='card'>
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
